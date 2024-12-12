b8e8b8cf5435eab7faa14d360644c3887cb703db
Raimdrs <raiimdrs@gmail.com>
2024-12-11 22:40:32 -0300

    fix: Ajeitando controller delete da task/id

--- GTP/src/main/java/com/gtp/controllers/GtpController.java
+++ GTP/src/main/java/com/gtp/controllers/GtpController.java
@@ -21,6 +21,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -100,4 +101,25 @@ public class GtpController {
         return tasks.stream().map(TaskDto::new).collect(Collectors.toList());
     }
 
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) {
+        Optional<UserModel> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable UUID id) {
+        Optional<TaskModel> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada.");
+        }
+    }
 }
